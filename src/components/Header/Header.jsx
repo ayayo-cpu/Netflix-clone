bef3f0737a53072236f6b0c4c190494db9a0e982
Ayanso Mitiku <77237684+ayayo-cpu@users.noreply.github.com>
2025-02-10 13:44:15 +0300

    header and Footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./Header.css"
+import NetflixLogo from "../../assets/images/logo.jpg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+function Header() {
+  return (
+    <div className="headerOuterContainer">
+      <div className="headerContainer">
+        <div className="headerLeft">
+          <ul>
+            <li><img src={NetflixLogo} alt="Netflix logo" /></li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Language</li>
+          </ul>
+        </div>
+        <div className="headerRight">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+                <NotificationsNoneIcon/>
+            </li>
+            <li>
+                <AccountBoxIcon/>
+            </li>
+            <li>
+                <ArrowDropDownIcon/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
